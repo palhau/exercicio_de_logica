133f8d798a38c26fcb4a1c46ab720e4ddb33cd3a
palhau <alan.palhau@truckpad.com.br>
2020-09-10 01:25:05 -0300

    refactor: refatoracao basica do codigo realizada

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-# Desafio de programação inicial
-
-# 1- O galão deve ser completamente preenchido com o volume das garrafas;
-# 2- Procure esvaziar totalmente as garrafas escolhidas;
-# 3- Quando não for possível esvaziar todas garrafas escolhidas, deixe a menor sobra possível;
-# 4- utilize o menor número de garrafas possível;
-
 def enchendo_galao():
+
     galao = float(input('Insira o volume do galão: '))
     qtd_garrafas = int(input('Insira a quantidade de garrafas: '))
     garrafas = list()
@@ -23,33 +17,32 @@ def enchendo_galao():
 
     garrafas_ordenadas = sorted(garrafas)
     garrafas_usadas = list()
-    valor_galao = galao
+    volume_galao = galao
+    sobra = 0
 
-    for map_garrafa in garrafas:
+    for _ in garrafas:
         proximo = 0
-        sobra = 0
 
         for garrafa in garrafas:
-            if (garrafa <= valor_galao) & (garrafa > proximo):
+            if (garrafa <= volume_galao) & (garrafa > proximo):
                 proximo = garrafa
 
         garrafas_usadas.append(proximo)
-        valor_galao -= proximo
+        volume_galao -= proximo
 
-        if valor_galao == 0:
+        if volume_galao == 0:
             break
 
     if sum(garrafas_usadas) < galao:
         for garrafa in garrafas_ordenadas:
-            if garrafa >= valor_galao:
+            if garrafa >= volume_galao:
                 garrafas_usadas.append(garrafa)
                 sobra = abs(sum(garrafas_usadas) - galao)
                 break
 
     garrafas_usadas = [filtro_garrafas for filtro_garrafas in garrafas_usadas if filtro_garrafas != 0]
 
-    print(garrafas_usadas)
-    print(f'Resposta: {garrafas_usadas}, sobra {sobra}L')
+    print(f'\n Resposta: {garrafas_usadas}, sobra {sobra}L')
 
 
 if __name__ == '__main__':
